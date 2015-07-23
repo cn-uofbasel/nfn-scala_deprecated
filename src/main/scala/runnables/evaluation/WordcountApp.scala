ee2910872842861c8b2b4a340d75789030c3bd9d
Christopher Scherb <christopher.scherb@unibas.ch>
2015-07-23 14:11:18 +0200

    Update travis

--- src/main/scala/runnables/evaluation/WordcountApp.scala
+++ src/main/scala/runnables/evaluation/WordcountApp.scala
@@ -3,10 +3,9 @@ package runnables.evaluation
 import ccn.packet.Interest
 import com.typesafe.config.{ConfigFactory, Config}
 import lambdacalculus.parser.ast.Expr
-import monitor.Monitor
 import nfn.service.{PandocTestDocuments, WordCount, Pandoc}
 import node.LocalNodeFactory
-
+import nfn.LambdaNFNImplicits._
 import concurrent.ExecutionContext.Implicits.global
 
 
@@ -21,7 +20,7 @@ object WordcountApp extends App{
   val node1 = LocalNodeFactory.forId(1)
   //  val node2 = LocalNodeFactory.forId(2)
   //  val nodes = List(node1, node2)
-  val nodes = List(node1)
+  //  val nodes = List(node1)
   //  node1 <~> node2
 
   val pandocServ = new Pandoc()
@@ -40,15 +39,12 @@ object WordcountApp extends App{
   //  node2.addPrefixFace(pandocServ.ccnName, node1)
 
   Thread.sleep(1000)
-  import lambdacalculus.parser.ast.LambdaDSL._
-  import nfn.LambdaNFNImplicits._
+
   implicit val useThunks: Boolean = false
 
   val exprWc: Expr = wc call (tutorialContent.name)
 
   sendAndPrintForName(exprWc)
-
-
 
   def sendAndPrintForName(interest: Interest) = {
     val startTime = System.currentTimeMillis
@@ -57,13 +53,11 @@ object WordcountApp extends App{
       case Success(resultContent) => {
         val runTime = System.currentTimeMillis - startTime
         println(s"RESULT:\n${new String(resultContent.data)}\nTIME: (${runTime}ms)")
-        Monitor.monitor ! Monitor.Visualize()
-        nodes foreach { _.shutdown() }
+        node1.shutdown()
         System.exit(0)
       }
       case Failure(e) => println(s"Could not receive content for $interest")
-        Monitor.monitor ! Monitor.Visualize()
-        nodes foreach { _.shutdown() }
+        node1.shutdown()
         System.exit(-1)
     }
 
