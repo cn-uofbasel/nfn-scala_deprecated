1e40ce099d2b531ff49c53d371b76b7e027c5f08
Christopher Scherb <christopher.scherb@unibas.ch>
2016-03-20 10:06:49 -0700

    Minor Bug fix in prefix of helpers for orgOpenmhealth

--- src/main/scala/orgOpenmhealth/helpers/Helpers.scala
+++ src/main/scala/orgOpenmhealth/helpers/Helpers.scala
@@ -12,20 +12,20 @@ import scala.concurrent.duration._
   */
 object Helpers {
 
-  def buildCatalogName(user:String, timestamp:String, prefix:String="org/openhealth", version:Int = 1):CCNName =
-    CCNName(prefix).append(user).append("data/fitness/physical_activity/time_location/catalog").append(timestamp)
+  def buildCatalogName(user:String, timestamp:String, prefix:String="org/openmhealth", version:Int = 1):CCNName =
+    CCNName(prefix.split('/').toList, None).append(user).append("data/fitness/physical_activity/time_location/catalog").append(timestamp)
 
-  def buildDataPointPacketName(user:String, timestamp:String, prefix:String="org/openhealth", version:Int = 1):CCNName =
-    CCNName(prefix).append(user).append("data/fitness/physical_activity/time_location").append(timestamp)
+  def buildDataPointPacketName(user:String, timestamp:String, prefix:String="org/openmhealth", version:Int = 1):CCNName =
+    CCNName(prefix.split('/').toList, None).append(user).append("data/fitness/physical_activity/time_location").append(timestamp)
 
-  def resolveDataPointPacket(ccnApi: ActorRef, user:String, timestamp:String, prefix:String="org/openhealth", version:Int = 1): String = {
+  def resolveDataPointPacket(ccnApi: ActorRef, user:String, timestamp:String, prefix:String="org/openmhealth", version:Int = 1): String = {
 
     val name = buildDataPointPacketName(user, timestamp)
    new String(fetchContent(Interest(name), ccnApi, 30 seconds).get.data)
 
   }
 
-  def resolveCatalog(ccnApi: ActorRef, user:String, timestamp:String, prefix:String="org/openhealth", version:Int = 1):List[String] = {
+  def resolveCatalog(ccnApi: ActorRef, user:String, timestamp:String, prefix:String="org/openmhealth", version:Int = 1):List[String] = {
 
     val catalogName = buildCatalogName(user, timestamp)
     val catalogData = new String(fetchContent(Interest(catalogName), ccnApi, 30 seconds).get.data)
@@ -35,8 +35,6 @@ object Helpers {
     (for  { t <- dataPointTimeStamps
       data = resolveDataPointPacket(ccnApi, user, t)
     } yield data).toList
-
-
   }
 
 
