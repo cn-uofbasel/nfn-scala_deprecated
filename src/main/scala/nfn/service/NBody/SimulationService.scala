36d5116d38f3f13ebd4f3c128f8c35ea24232a8b
Balázs Faludi <balazsfaludi@gmail.com>
2017-04-19 16:04:48 +0200

    Add config size option to simulation service.

--- src/main/scala/nfn/service/NBody/SimulationService.scala
+++ src/main/scala/nfn/service/NBody/SimulationService.scala
@@ -9,13 +9,14 @@ import nfn.tools.Networking._
 class SimulationService extends NFNService {
 
   override def function(interestName: CCNName, argSeq: Seq[NFNValue], ccnApi: ActorRef): NFNValue = {
-    // /path/to/NBodySimulation /path/to/config '-d' <deltaTime> '-s' <stepCount> '-i' <intermediateInterval>
+    // /path/to/NBodySimulation [/path/to/config] ['-c' <configSize>] ['-d' <deltaTime>] ['-s' <stepCount>] ['-i' <intermediateInterval>]
 
-    var options = Map('deltaTime -> 60, 'stepCount -> 1000, 'intermediateInterval -> 0)
+    var options = Map('configSize -> 10, 'deltaTime -> 60, 'stepCount -> 1000, 'intermediateInterval -> 0)
     var configuration = Array[Byte]()
 
     var args = argSeq.toList
     while (args.nonEmpty) args match {
+      case NFNStringValue("-c") :: NFNIntValue(value) :: tail => options += ('configSize -> value); args = args drop 2
       case NFNStringValue("-d") :: NFNIntValue(value) :: tail => options += ('deltaTime -> value); args = args drop 2
       case NFNStringValue("-s") :: NFNIntValue(value) :: tail => options += ('stepCount -> value); args = args drop 2
       case NFNStringValue("-i") :: NFNIntValue(value) :: tail => options += ('intermediateInterval -> value); args = args drop 2
@@ -23,6 +24,7 @@ class SimulationService extends NFNService {
       case _ => args = List()
     }
 
+    val configSize = options('configSize)
     val deltaTime = options('deltaTime)
     val stepCount = options('stepCount)
     val intermediateInterval = options('intermediateInterval) * 1000
@@ -31,7 +33,7 @@ class SimulationService extends NFNService {
 
     val systemSize = Vector(Body.earth.radius * 500, Body.earth.radius * 500)
     val renderArea = Rect(-systemSize / 2, systemSize)
-    val config = if (configuration.length <= 0) Config.random(renderArea, 10)
+    val config = if (configuration.length <= 0) Config.random(renderArea, configSize)
       else Config.fromString(configuration.toString)
 
     val simulation = new Simulation(config, deltaTime)
