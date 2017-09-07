6dc819e3003a417a8562c8cfd9d37a78d9da4102
DMI-Scherb Christopher <christopher.scherb@unibas.ch>
2017-09-07 17:51:05 +0200

    add chaining

--- src/main/scala/nfn/service/SensorDataProcessingService.scala
+++ src/main/scala/nfn/service/SensorDataProcessingService.scala
@@ -9,11 +9,14 @@ import scala.io.Source
 class SensorDataProcessingService() extends  NFNService {
 
   override def function(interestName: CCNName, args: Seq[NFNValue], ccnApi: ActorRef): NFNValue = {
+    //return NFNStringValue(args.tail.head.getClass.toString)
     args match {
-     case Seq(num: NFNIntValue, data: NFNStringValue) => {
-       val list = data.str.split("\n")
+
+     case Seq(num: NFNIntValue, data: NFNContentObjectValue) => {
+       val list = new String(data.data).split("\n")
        NFNStringValue(list.toList(num.i))
       }
+     case _ => NFNStringValue("error")
     }
   }
 }
