252fbcdd2655d772dac18f6861b5f7f59c262e48
DMI-Scherb Christopher <christopher.scherb@unibas.ch>
2017-09-07 17:09:53 +0200

    name object service is running

--- src/main/scala/nfn/service/ListIntervalService.scala
+++ src/main/scala/nfn/service/ListIntervalService.scala
@@ -12,24 +12,22 @@ class ListIntervalService() extends  NFNService {
   override def pinned = true
 
   override def function(interestName: CCNName, args: Seq[NFNValue], ccnApi: ActorRef): NFNValue = {
-    NFNStringValue(
-    /*args match {
-      case Seq(start: NFNLongValue, end: NFNLongValue) => {
+
+    args match {
+     case Seq(start: NFNIntValue, end: NFNIntValue) => {
 
         //TODO change start and end from time to unix
-        val uStart = start.l
-        val uEnd = end.l
+        val uStart = start.i
+        val uEnd = end.i
 
         val list =  Source.fromFile(filename).getLines.toList.map(s => new CCNName(s.split("/").tail.toList, None))
         val point = list.filter(v => v.cmps.last.toLong <= uEnd && v.cmps.last.toLong >= uStart)
 
-        val string : String = point.foldLeft("") (_ + "\n" + _.toString)
+        //return NFNStringValue(point.head.toString)
 
+        val string : String = point.foldLeft("_") (_ + "\n" + _.toString)
         return NFNStringValue(string)
       }
-
-    }*/
-      "HALLOWELT1234"
-    )
+    }
   }
 }
