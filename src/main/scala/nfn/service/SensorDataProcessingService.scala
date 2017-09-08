65f0c505b3584fa2896d4d50871e4b13e9def01f
DMI-Scherb Christopher <christopher.scherb@unibas.ch>
2017-09-08 17:22:06 +0200

    get data from network

--- src/main/scala/nfn/service/SensorDataProcessingService.scala
+++ src/main/scala/nfn/service/SensorDataProcessingService.scala
@@ -1,7 +1,9 @@
 package nfn.service
 
 import akka.actor.ActorRef
-import ccn.packet.CCNName
+import ccn.packet.{CCNName, Interest}
+import nfn.tools.Networking._
+import scala.concurrent.duration._
 
 import scala.io.Source
 
@@ -13,8 +15,15 @@ class SensorDataProcessingService() extends  NFNService {
     args match {
 
      case Seq(num: NFNIntValue, data: NFNContentObjectValue) => {
-       val list = new String(data.data).split("\n")
-       NFNStringValue(list.toList(num.i))
+       val list = new String(data.data).split("\n").toList
+
+       //request data from remote host here
+       val i1 = new Interest( new CCNName(list(num.i).split("/").toList.tail, None))
+       val c1 = fetchContent(i1, ccnApi, 30 seconds).get
+
+       val cont = new String(c1.data)
+
+       NFNStringValue(cont)
       }
      case _ => NFNStringValue("error")
     }
